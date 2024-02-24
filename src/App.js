c74c45ff881cbae80b7d4ab8ecc0d530d6b77901
Zac Valant <zac.valant@baader.com>
2024-02-24 06:13:38 -0600

    modify tree to have nodes yellow if all children demands are complete and green if nodes themselves are complete

--- src/App.js
+++ src/App.js
@@ -17,50 +17,19 @@ const renderCard = ({ nodeDatum, toggleNode, foreignObjectProps = {} }) => {
     <React.Fragment>
       <foreignObject
         {...foreignObjectProps}
-        width="200"
+        width="350"
         height="500"
         x="-100"
         y="-10"
       >
         <div
-          className={`card ${ nodeDatum.attributes.isDemandMet? "card--is-company" : ""} ${
-            hasChildren ? "card--has-children" : ""
-          }`}
+          className={`card ${nodeDatum.attributes.isComponentDemandMet?"componenet-demand-met":""} ${ nodeDatum.attributes.isDemandMet? "demand-met" : ""}     `}
         >
-          
           <h2>{nodeDatum.name}</h2>
-          <p>DESCRIPTION: {nodeDatum.attributes?.description}</p>
+          <p className="desc">DESC: {nodeDatum.attributes?.description}</p>
           <p>QTY REQ: {nodeDatum.attributes.qty}</p>
           <p>ON HAND: {nodeDatum.attributes.onHand}</p>
 
-
-          {hasChildren? (
-            <button className="toggle" type="button" onClick={toggleNode}>
-              {nodeDatum.__rd3t.collapsed ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  stroke="none"
-                  fill="currentcolor"
-                >
-                  <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  stroke="none"
-                  fill="currentcolor"
-                >
-                  <path d="M0 10h24v4h-24z" />
-                </svg>
-              )}
-            </button>
-          ) : null}
         </div>
       </foreignObject>
     </React.Fragment>
@@ -76,26 +45,21 @@ export default function App() {
   return (
     <>
       {JSON.stringify(cardCoords)}
-      <p className="colors">
-        <span className="company">Company</span>{" "}
-        <span className="clinic">Clinic</span>
-      </p>
       <div style={containerStyles} ref={containerRef}>
-
         <Tree
-          pathFunc="elbow"
-          separation={{ siblings: 1.5, nonSiblings: 2 }}
+          nodeSize={{x: 200, y: 800
+          }}
+          
+          pathFunc="beizer curve"
+          separation={{ siblings: 3, nonSiblings: 6 }}
           data={orgChartJson}
           dimensions={dimensions}
           translate={translate}
           orientation = "vertical"
-   
-          //initialDepth={0}
           centeringTransitionDuration={200}
           renderCustomNodeElement={(rd3tProps) =>
             renderCard({ ...rd3tProps, handleClick })
           }
-          
         />
       </div>
     </>
